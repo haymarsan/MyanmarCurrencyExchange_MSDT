ea972adde6ac95fd7936efd2036bc3d962ff5b1e
Hay Mar San <haymarsan11@gmail.com>
2020-01-02 11:05:46 +0630

    initial commit

---
+++ app/src/main/java/com/haymarsan/myanmarcurrencyexchange/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package com.haymarsan.myanmarcurrencyexchange.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
